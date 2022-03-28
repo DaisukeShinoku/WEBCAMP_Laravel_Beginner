16f372bdc5dbbc3af832353eef2918a58c122e0b
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2022-03-28 23:45:21 +0900

    finCh06

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginPostRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
@@ -25,6 +27,23 @@ class AuthController extends Controller
   {
     // データの取得
     $datum = $request->validated();
-    var_dump($datum); exit;
+    // var_dump($datum); exit;
+
+    // 認証
+    if (Auth::attempt($datum) === false) {
+      return back()
+            ->withInput()                                               // 入力値の保持
+            ->withErrors(['auth'=>'emailかパスワードに誤りがあります。',])   // エラーメッセージの出力
+            ;
+    }
+    $request->session()->regenerate();
+    return redirect()->intended('/task/list');
+  }
+  public function logout(Request $request)
+  {
+    Auth::logout();
+    $request->session()->regenerateToken(); // CSRFトークンの再生成
+    $request->session()->regenerate();      // セッションIDの再生成
+    return redirect(route('login'));
   }
 }
