3648b552d2e2cc4594b8d309ce493d608a77135a
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2022-04-03 12:35:13 +0900

    finCh07

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,6 +3,9 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\TaskRegisterPostRequest;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Task as TaskModel;
 
 class TaskController extends Controller
 {
@@ -14,5 +17,28 @@ class TaskController extends Controller
   public function list()
   {
     return view('task.list');
+  }
+  /**
+   * タスクの新規登録
+   */
+  public function register(TaskRegisterPostRequest $request)
+  {
+    // validate済のデータの取得
+    $datum = $request->validated();
+
+    // user_idの追加
+    $datum['user_id'] = Auth::id();
+
+    // テーブルへのINSERT
+    try {
+      $r = TaskModel::create($datum);
+    } catch(\Throwable $e) {
+      echo $e->getMessage();
+      exit;
+    }
+
+    // タスク登録成功
+    $request->session()->flash('front.task_register_success', true);
+    return redirect('/task/list');
   }
 }
