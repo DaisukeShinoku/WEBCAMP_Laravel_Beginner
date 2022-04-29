39ca7d3bb8697c994117e21f047afe50c0dd4120
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2022-04-30 00:10:58 +0900

    finCh13

---
+++ resources/views/admin/top.blade.php
@@ -0,0 +1,14 @@
+@extends('admin.layout')
+
+@section('contets')
+  <menu label="リンク">
+    <a href="./user_list.html">ユーザ一覧</a><br>
+    管理画面機能1<br>
+    管理画面機能2<br>
+    管理画面機能3<br>
+    管理画面機能4<br>
+    <a href="/admin/logout">ログアウト</a>
+  </menu>
+  <h1>管理画面</h1>
+  (アクセス傾向のグラフや警告などを表示する事が多い)<br>
+@endsection
